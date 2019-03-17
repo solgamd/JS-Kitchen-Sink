506a66813e50a561051acdc5bea1452ce0b22527
solgamd <solgamd@gmail.com>
2019-03-17 20:05:51 +0100

    Updated: through task #8

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-function s1() 
-{
+//My name
+var name = "Megan";
 
-    var fruits = document.getElementById("myInput")
-    .value;
+//Number of states in USA
+const states = 52;
 
-    switch(fruits) 
-    {
-    case "1":
-        document.write("" + "This is first choice!" + "");
-        break;
-    case "2":
-        document.write("" + "This is second choice!" + "");
-        break;
-    case "3":
-        document.write("" + "This is third choice!" + "");
-        break;
-    default:
-        document.write("" + "Wrong choice! Try again" + "");
+//Add 5 and 4
+var add = 5 + 4;
 
+ function sayHello() {
+    alert ("Hello World!") ;
+};
+
+sayHello();
+
+function checkAge(name, age) {
+    if(age => 21) {
+        return "Welcome, " + name + "!";
+    } else if(age < 21) {
+         return "Sorry " + name + ", you aren't old enough to view this page!";
     }
+};
 
-}
+var output = checkAge('Charles', 21);
+    console.log(output);
+var output = checkAge('Abby', 27);
+    console.log(output);    
+var output = checkAge('James', 18);
+    console.log(output);  
+var output = checkAge('John', 17);
+    console.log(output);
